8800418d2f9559ab0a555ab79e2d084b2b8bae85
Md Antor <md.antor1155@gmail.com>
2022-04-01 14:41:13 +0800

    added react spring but don't work

--- react-rechart/src/App.js
+++ react-rechart/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Chart from './components/Chart/Chart';
+import SpringAnimation from './components/SpringAnimation/SpringAnimation';
 
 function App() {
   return (
     <div className="App">
+      <SpringAnimation></SpringAnimation>
      <Chart></Chart>
     </div>
   );
---
+++ react-rechart/src/components/SpringAnimation/SpringAnimation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+
+const SpringAnimation = () => {
+    const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+    return (
+    <animated.div style={props}>I will fade in</animated.div>
+    );
+};
+
+export default SpringAnimation;
